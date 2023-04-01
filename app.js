6964253d4393a43fb4c1c37192083c5d288d1537
Reza Ghanbari <reza001391@gmail.com>
2023-04-01 17:25:49 +0430

    Create a function to add tasks

---
+++ app.js
@@ -0,0 +1,48 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+// Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+
+// FUnctions
+
+function addTodo(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // create div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  todoDiv.innerHTML = `
+  <li class="todo-item">${todoInput.value}</li>
+  <button class = "complete-btn"><i class='fas fa-check'></i></button>
+  <button class = "trash-btn"><i class='fas fa-trash'></i></button>
+  `;
+  //   append to ul
+  todoList.appendChild(todoDiv);
+  //   clear todo input value
+  todoInput.value = "";
+  //   Another way to add
+  //   create div
+  //   const todoDiv = document.createElement("div");
+  //-  todoDiv.classList.add("todo");
+  //   create li
+  //   const newTodo = document.createElement("li");
+  //   newTodo.innerText = `hey`;
+  //   newTodo.classList.add("todo-item");
+  //-  todoDiv.appendChild(newTodo);
+  //   check mark button
+  //   const completedButton = document.createElement("button");
+  //   completedButton.innerHTML = "<i class='fas fa-check'></i>";
+  //   completedButton.classList.add("complete-btn");
+  //-  todoDiv.appendChild(completedButton);
+  //   check trash button
+  //   const trashButton = document.createElement("button");
+  //   trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+  //   trashButton.classList.add("trash-btn");
+  // - todoDiv.appendChild(trashButton);
+  //   append to list
+  //-  todoList.appendChild(todoDiv);
+  //   console.log(todoList);
+}
